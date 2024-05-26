2edb168f5e1a21360c5da87f51524144849bf09e
Cyan <2376302102@qq.com>
2024-05-26 21:44:40 +0800

    fix typo, change KFS_EXT4_DISK_MIN_SIZE to 512KiB, change MKFS_EXT4_INODE_RATIO to 1024

--- mkfs/config.h
+++ mkfs/config.h
@@ -1,13 +1,12 @@
+#define KiB                       1024
+#define MiB                       (KiB * 1024)
 
-#define KB                        1024
-#define MB                        (KB * 1024)
-
-#define MKFS_EXT4_DISK_MIN_SIZE   (128 * MB)
+#define MKFS_EXT4_DISK_MIN_SIZE   (512 * KiB)
 
 #define MKFS_EXT4_LOG_BLOCK_SIZE  2
 #define MKFS_EXT4_BLOCK_SIZE      ((uint64_t)1 << (MKFS_EXT4_LOG_BLOCK_SIZE + 10))  // 4096
 #define MKFS_EXT4_GROUP_BLOCK_CNT (MKFS_EXT4_BLOCK_SIZE * 8)
 #define MKFS_EXT4_INODE_SIZE      256
-#define MKFS_EXT4_INODE_RATIO     16384
+#define MKFS_EXT4_INODE_RATIO     1024
 #define MKFS_EXT4_DESC_SIZE       64
 #define MKFS_EXT4_RESV_DESC_CNT   0
